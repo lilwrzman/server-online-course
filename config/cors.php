afcc4eed7877ead72ffd5c590441442d9c92bef8
lilwrzman <egydya.edh12@gmail.com>
2024-07-07 00:08:27 +0700

    allow online-course.pocari.id to cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://online-course.pocari.id'],
 
     'allowed_origins_patterns' => [],
 
