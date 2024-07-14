4166f4c33fbc74a678585a7f9fa0ce7166e48888
lilwrzman <egydya.edh12@gmail.com>
2024-07-14 23:56:07 +0700

    dashboard student 8

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -39,6 +39,7 @@ class DashboardController extends Controller
 
             foreach($courses as $course){
                 $course->latest_progress = $latestProgresses->firstWhere('item.course_id', $course->id);
+                $course->feedback = $course->feedbacks->firstWhere('user_id', $user->id);
                 $course->makeHidden(['items']);
             }
 
