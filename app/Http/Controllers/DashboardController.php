db9db16e4e5e3283f195be8fd0b513319ff68a44
lilwrzman <egydya.edh12@gmail.com>
2024-07-15 00:04:36 +0700

    dashboard student 15

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -38,11 +38,10 @@ class DashboardController extends Controller
                         }])->get(['id', 'title']);
 
             foreach($courses as $course){
-                $course->latest_progress = $latestProgresses
-                                            ->firstWhere('item.course_id', $course->id)
-                                            ->get(['id', 'course_id', 'title', 'type']);
+                $course->latest_progress = $latestProgresses->firstWhere('item.course_id', $course->id);
                 $course->feedback = $course->feedbacks->firstWhere('user_id', $user->id);
                 $course->makeHidden(['items', 'feedbacks']);
+                $course->latest_progress->makeHidden(['description', 'slug', 'info', 'order', 'created_at', 'updated_at']);
             }
 
             return response()->json(['status' => true, 'data' => $courses]);
